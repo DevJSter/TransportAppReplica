a8a90f007261ff2f8f8957fce9216ee8083454da
DevJSter <shubht3303@gmail.com>
2023-08-30 23:23:28 +0530

    First commit

---
+++ UserApp/src/components/RouteMap/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+
+const GOOGLE_MAPS_APIKEY = 'AIzaSyDFhFUaYpyAjNE4Eq-sWCGWjrr6kyGnhbQ';
+
+const RouteMap = ({ origin, destination }) => {
+
+  const originLoc = {
+    latitude: origin.details.geometry.location.lat,
+    longitude: origin.details.geometry.location.lng,
+  };
+
+  const destinationLoc = {
+    latitude: destination.details.geometry.location.lat,
+    longitude: destination.details.geometry.location.lng,
+  };
+
+  return (
+    <MapView
+      style={{width: '100%', height: '100%'}}
+      provider={PROVIDER_GOOGLE}
+      showsUserLocation={true}
+      initialRegion={{
+        latitude: 28.450627,
+        longitude: -16.263045,
+        latitudeDelta: 0.0222,
+        longitudeDelta: 0.0121,
+      }}>
+      <MapViewDirections
+        origin={originLoc}
+        destination={destinationLoc}
+        apikey={GOOGLE_MAPS_APIKEY}
+        strokeWidth={5}
+        strokeColor="black"
+      />
+      <Marker
+        coordinate={origin}
+        title={'Origin'}
+      />
+      <Marker
+        coordinate={destination}
+        title={"Destination"}
+      />
+    </MapView>
+  );
+};
+
+export default RouteMap;
